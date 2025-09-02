d0e084b56549b8b6895a94c3c144159010dd68be
jiteshcelsys <Jitesh.p@celestialsys.com>
2025-09-02 15:16:09 +0530

    ApplicationReview

--- src/components/Application/ApplicationReview.tsx
+++ src/components/Application/ApplicationReview.tsx
@@ -15,6 +15,7 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useApplication } from "../../context/ApplicationContext";
+import { FileText } from "lucide-react";
 
 const steps = [
   "Personal Details",
